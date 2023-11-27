dd9e8ea2f7979dcf20539f64afabc4234b554072
YanLucass <yanlucascarvalho18@gmail.com>
2023-11-27 15:35:40 -0300

    finalizando validações e criação de usuario

--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import UserController from '../controllers/UserController';
-const router = express.Router();
-
-router.get('/register', UserController.register);
-
-export default router;
